e34d6e9a890234bb080d2e1a1acb37d2c7fe7e42
Dan Fink <Daniel.Fink@cognizant.com>
2024-11-18 09:01:09 -0800

    Remove leaf-common as name of SDK

--- neuro_san/graph/registry/name_correction_config_filter.py
+++ neuro_san/graph/registry/name_correction_config_filter.py
@@ -6,7 +6,7 @@
 # You can be released from the terms, and requirements of the Academic Public
 # License by purchasing a commercial license.
 # Purchase of a commercial license is mandatory for any use of the
-# leaf-common SDK Software in commercial settings.
+# neuro-san SDK Software in commercial settings.
 #
 # END COPYRIGHT
 """
