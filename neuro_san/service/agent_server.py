4a1abbc8742b574c355b83cdef1605bc9d344015
Dan Fink <Daniel.Fink@cognizant.com>
2024-11-21 12:23:49 -0800

    Fix Path Traversal and some references to DECISION_ASSISTANT env vars

--- neuro_san/service/agent_server.py
+++ neuro_san/service/agent_server.py
@@ -66,8 +66,8 @@ class AgentServer:
         current_dir = os.path.dirname(os.path.abspath(__file__))
 
         setup_logging(SERVER_NAME_FOR_LOGS, current_dir,
-                      'DECISION_ASSISTANT_SERVICE_LOG_JSON',
-                      'DECISION_ASSISTANT_SERVICE_LOG_LEVEL')
+                      'AGENT_SERVICE_LOG_JSON',
+                      'AGENT_SERVICE_LOG_LEVEL')
         # This module within openai library can be quite chatty w/rt http requests
         logging.getLogger("httpx").setLevel(logging.WARNING)
 
