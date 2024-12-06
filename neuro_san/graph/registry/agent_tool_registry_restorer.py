f76d7672532140f27944b9bb0f1921333109ed80
Dan Fink <Daniel.Fink@cognizant.com>
2024-12-06 14:36:09 -0800

    Remove commented out line

--- neuro_san/graph/registry/agent_tool_registry_restorer.py
+++ neuro_san/graph/registry/agent_tool_registry_restorer.py
@@ -60,9 +60,6 @@ class AgentToolRegistryRestorer(Restorer):
         if file_reference is None or len(file_reference) == 0:
             raise ValueError(f"file_reference {file_reference} cannot be None or empty string")
 
-        # if file_reference.startswith("/") and self.registry_dir is not None:
-        #     raise ValueError(f"file_reference {file_reference} cannot be an absolute path")
-
         use_file: str = file_reference
         if self.registry_dir is not None:
             use_file = path.join(self.registry_dir, file_reference)
