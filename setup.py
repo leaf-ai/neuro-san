54e01480ceab832da9c17be1f570880a457fc159
Dan Fink <Daniel.Fink@cognizant.com>
2024-12-12 08:37:38 -0800

    Try a minimal setup.py for BlackDuck scans to go better:

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+# Per https://ianhopkinson.org.uk/2022/02/understanding-setup-py-setup-cfg-and-pyproject-toml-in-python/
+# a minimal setup.py file to get other tools happy with the pyproject.toml
+
+if __name__ == "__main__":
+    setup()
