94ce4a3383c944bc300c10185ad1c847fe2f580b
Dan Fink <Daniel.Fink@cognizant.com>
2024-12-24 10:00:56 -0800

    Use AtomicCounter from leaf-server-common

--- neuro_san/service/agent_service.py
+++ neuro_san/service/agent_service.py
@@ -22,6 +22,7 @@ from google.protobuf.json_format import MessageToDict
 from google.protobuf.json_format import Parse
 
 from leaf_server_common.asyncio.asyncio_executor import AsyncioExecutor
+from leaf_server_common.server.atomic_counter import AtomicCounter
 from leaf_server_common.server.grpc_metadata_forwarder import GrpcMetadataForwarder
 from leaf_server_common.server.request_logger import RequestLogger
 
@@ -29,7 +30,6 @@ from neuro_san.api.grpc import agent_pb2 as service_messages
 from neuro_san.api.grpc import agent_pb2_grpc
 
 from neuro_san.graph.registry.agent_tool_registry import AgentToolRegistry
-from neuro_san.service.atomic_counter import AtomicCounter
 from neuro_san.session.chat_session_map import ChatSessionMap
 from neuro_san.session.direct_agent_session import DirectAgentSession
 
