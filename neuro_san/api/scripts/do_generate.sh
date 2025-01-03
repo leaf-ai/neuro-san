7c30132d00ba5ec2ffaed5aa5d447a11629836be
Dan Fink <Daniel.Fink@cognizant.com>
2025-01-02 17:39:37 -0800

    Update copyrights to 2025

--- neuro_san/api/scripts/do_generate.sh
+++ neuro_san/api/scripts/do_generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2023-2024 Cognizant Digital Business, Evolutionary AI.
+# Copyright (C) 2023-2025 Cognizant Digital Business, Evolutionary AI.
 # All Rights Reserved.
 # Issued under the Academic Public License.
 #
