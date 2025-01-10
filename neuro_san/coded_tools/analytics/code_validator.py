59ec337bb42b7975ffd45fc23514690201dd9784
deepsaia <deepak.singh10@cognizant.com>
2025-01-09 17:55:38 -0800

    add hocon

---
+++ neuro_san/coded_tools/analytics/code_validator.py
@@ -0,0 +1,72 @@
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Union
+
+import pandas as pd
+
+from neuro_san.interfaces.coded_tool import CodedTool
+
+
+class CodeValidator(CodedTool):
+    """
+    CodedTool implementation which handles validating a code snippet.
+    """
+
+    def invoke(self, args: Dict[str, Any], sly_data: Dict[str, Any]) -> Union[Dict[str, Any], str]:
+        """
+        Called when the coded tool is invoked by the agent hierarchy.
+
+        :param args: An argument dictionary whose keys are the parameters
+                to the coded tool and whose values are the values passed for them
+                by the calling agent.  This dictionary is to be treated as read-only.
+
+                The argument dictionary expects the following keys:
+                    "code_snippet":  Python code snippet to run for synthetic data generation.
+
+        :param sly_data: A dictionary whose keys are defined by the agent hierarchy,
+                but whose values are meant to be kept out of the chat stream.
+
+                This dictionary is largely to be treated as read-only.
+                It is possible to add key/value pairs to this dict that do not
+                yet exist as a bulletin board, as long as the responsibility
+                for which coded_tool publishes new entries is well understood
+                by the agent chain implementation and the coded_tool implementation
+                adding the data is not invoke()-ed more than once.
+
+                Keys expected for this implementation are:
+                    None
+
+        :return:
+            In case of successful execution:
+                a dictionary containing S3 URI of resulting output,
+                which includes the following key:
+                    "output_url"
+            otherwise:
+                a text string an error message in the format:
+                "Error: <error message>"
+        """
+        # Formulate the file reference for the request
+        code_snippet = args.get("code_snippet")
+
+        # Validate the code
+        is_valid, error_message = validate_code(code_snippet)
+        if not is_valid:
+            return error_message
+        else:
+            return code_snippet
+
+    # Function to validate matplotlib code
+    def validate_code(code):
+        try:
+            # Check for unsafe imports or malicious code
+            disallowed_imports = ["io", "os", "subprocess", "sys", "importlib", "pickle", "shutil", "tempfile"]
+            for disallowed in disallowed_imports:
+                if disallowed in code:
+                    raise ValueError(f"Disallowed import detected: {disallowed}")
+            compile(code, '<string>', 'exec')
+            print("Code validation successful.")
+        except (SyntaxError, ValueError) as e:
+            print(f"Code validation failed: {e}")
+            return False, str(e)
+        return True, None
---
+++ neuro_san/coded_tools/analytics/get_input_file.py
@@ -0,0 +1,63 @@
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Union
+
+import pandas as pd
+import os
+
+from neuro_san.interfaces.coded_tool import CodedTool
+
+
+class GetInputFile(CodedTool):
+    """
+    CodedTool implementation which reads an input CSV file.
+    """
+    def __init__(self):
+        self.default_path = "./neuro_san/coded_tools/analytics-san/credit_risk_data_20250109.csv"
+
+    def invoke(self, args: Dict[str, Any], sly_data: Dict[str, Any]) -> Union[Dict[str, Any], str]:
+        """
+        Called when the coded tool is invoked by the agent hierarchy.
+
+        :param args: An argument dictionary whose keys are the parameters
+                to the coded tool and whose values are the values passed for them
+                by the calling agent.  This dictionary is to be treated as read-only.
+
+                The argument dictionary expects the following keys:
+                    "input_path": The input path to a CSV file
+
+        :param sly_data: A dictionary whose keys are defined by the agent hierarchy,
+                but whose values are meant to be kept out of the chat stream.
+
+                This dictionary is largely to be treated as read-only.
+                It is possible to add key/value pairs to this dict that do not
+                yet exist as a bulletin board, as long as the responsibility
+                for which coded_tool publishes new entries is well understood
+                by the agent chain implementation and the coded_tool implementation
+                adding the data is not invoke()-ed more than once.
+
+                Keys expected for this implementation are:
+                    None
+
+        :return:
+            In case of successful execution:
+                a path to the input CSV data file
+            otherwise:
+                a text string an error message in the format:
+                "Error: <error message>"
+        """
+        # Formulate the file reference for the request
+        input_path = input("Provide path to the input CSV (to use a sample data, press enter): ")
+        if not input_path:
+            input_path = self.default_path
+
+        if input_path.startswith("s3://"):
+            return input_path
+        # Load the dataset
+        if os.path.isabs(input_path):
+            return input_path
+        else:
+            return os.path.abspath(input_path)
+
+
