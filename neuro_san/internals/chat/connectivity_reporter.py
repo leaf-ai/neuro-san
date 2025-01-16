18bc395cbfeac47befe85b33629883993cd5ceed
Dan Fink <Daniel.Fink@cognizant.com>
2025-01-16 11:47:58 -0800

    Do a breadth-first search

--- neuro_san/internals/chat/connectivity_reporter.py
+++ neuro_san/internals/chat/connectivity_reporter.py
@@ -22,6 +22,7 @@ from neuro_san.internals.graph.registry.agent_tool_registry import AgentToolRegi
 from neuro_san.internals.graph.tools.front_man import FrontMan
 from neuro_san.internals.journals.journal import Journal
 from neuro_san.internals.messages.agent_framework_message import AgentFrameworkMessage
+from neuro_san.run_context.utils.external_tool_adapter import ExternalToolAdapter
 
 
 class ConnectivityReporter:
@@ -55,73 +56,103 @@ class ConnectivityReporter:
         """
         Share the connectivity information of the agent network in question
         """
-        # Get the list of agent names from the registry
-        agent_names: List[str] = self.registry.get_agent_names()
 
-        # Find out what the front man is called.
+        # Do a breadth-first traversal starting with the front-man
         front_man_spec: Dict[str, Any] = self.front_man.get_agent_tool_spec()
         front_man_name: str = self.registry.get_name_from_spec(front_man_spec)
 
-        # Be sure the front man goes first in the list.
-        # Remove it from the set, then insert at the beginning of the remaining list.
-        agent_name_set: Set[str] = set(agent_names)
-        agent_name_set.discard(front_man_name)
-        agent_names = list(agent_name_set)
-        agent_names.insert(0, front_man_name)
+        reported_agents: Set[str] = set()
+        await self.report_node_connectivity(front_man_name, reported_agents)
 
-        # Report connectivity for each node in the graph
-        for agent_name in agent_names:
-            agent_spec: Dict[str, Any] = self.registry.get_agent_tool_spec(agent_name)
-            await self.report_node_connectivity(agent_spec)
-
-    async def report_node_connectivity(self, agent_spec: Dict[str, Any]):
+    async def report_node_connectivity(self, agent_name: str, reported_agents: Set[str]):
         """
         Share the connectivity information of a single node in the network.
-        :param agent_spec: The agent spec dictionary to report on
+        :param agent_name: The name of the agent spec dictionary to report on
+        :param reported_agents: A list of agents that have been reported already.
+                Prevents cycles.
         """
-        extractor = DictionaryExtractor(agent_spec)
 
-        # Check to see if this node in the graph actually wants its connectivity
-        # known to the outside world.
-        allow_connectivity = bool(extractor.get("allow.connectivity", True))
-        if not allow_connectivity:
-            # Nothing to see here, please move along
-            return
+        agent_spec: Dict[str, Any] = None
 
-        # Keep a set of the combined sources of tools, so connectivity only gets
-        # listed once.
-        tool_set: Set[str] = set()
+        if not ExternalToolAdapter.is_external_agent(agent_name):
 
-        # First check the tools of the run-of-the-mill agent spec
-        empty_list: List[str] = []
-        tools: List[str] = agent_spec.get("tools", empty_list)
-        tool_set.update(tools)
+            # This is not an external agent, so get its spec to report on
+            agent_spec: Dict[str, Any] = self.registry.get_agent_tool_spec(agent_name)
+            extractor = DictionaryExtractor(agent_spec)
 
-        # Next check a special case convention where a coded tool takes a dictionary of
-        # key/value pairs mapping function -> tool name.
-        empty_dict: Dict[str, Any] = {}
-        args_tools = extractor.get("args.tools", empty_dict)
-        if isinstance(args_tools, Dict):
-            args_tools = args_tools.values()
-        if isinstance(args_tools, List):
-            tool_set.update(args_tools)
+            # Check to see if this node in the graph actually wants its connectivity
+            # known to the outside world.
+            allow_connectivity = bool(extractor.get("allow.connectivity", True))
+            if not bool(allow_connectivity):
 
-        # Make a list from the set of tools
-        tool_list: List[str] = list(tool_set)
+                # We are not allowing connectivity reporting from here on down
+                agent_spec = None
 
-        # Recall that an empty list evaluates to False
-        if not bool(tool_list):
-            # Nothing to see here, please move along
-            return
+        # Compile a tool list of what is referred to by the agent_spec.
+        # For many reasons, this list could be empty.
+        tool_list: List[str] = self.assemble_tool_list(agent_spec)
 
         # Report the content of the tools list as a dictionary in JSON.
         tools_dict: Dict[str, Any] = {
             "tools": tool_list
         }
-        content: str = json.dumps(tools_dict)
+        content: str = f"```json\n{json.dumps(tools_dict)}```"
         message = AgentFrameworkMessage(content=content)
 
         # Report the origin as the agent itself, so any client that receives
         # the message has the correct context about the tools listed in the content.
-        agent_name: str = self.registry.get_name_from_spec(agent_spec)
         await self.journal.write_message(message, origin=agent_name)
+        reported_agents.add(agent_name)
+
+        # Recurse for a bread-first search.
+        for tool in tool_list:
+            # Don't report more than once for the same node to avoid cycles.
+            if tool not in reported_agents:
+                await self.report_node_connectivity(tool, reported_agents)
+
+    @staticmethod
+    def assemble_tool_list(agent_spec: Dict[str, Any]) -> List[str]:
+        """
+        :param agent_spec: The agent spec to assemble a tool list from.
+        :return: A list of tool names referred to by the agent spec.
+        """
+
+        # Keep a list of the tools to respect the ordering they were specified.
+        tool_list: List[str] = []
+
+        if agent_spec is None:
+            # Nothing to report
+            return tool_list
+
+        extractor = DictionaryExtractor(agent_spec)
+
+        # Keep a set of the combined sources of tools,
+        # so connectivity only gets reported once.
+        tool_set: Set[str] = set()
+
+        # First check the tools of the run-of-the-mill agent spec
+        empty_list: List[str] = []
+        spec_tools: List[str] = extractor.get("tools", empty_list)
+
+        # Add to our list in order, but without repeats
+        for tool in spec_tools:
+            if tool not in tool_set:
+                tool_set.add(tool)
+                tool_list.append(tool)
+
+        # Next check a special case convention where a coded tool takes a dictionary of
+        # key/value pairs mapping function -> tool name.
+        empty_dict: Dict[str, Any] = {}
+        args_tools = extractor.get("args.tools", empty_dict)
+
+        if isinstance(args_tools, Dict):
+            args_tools = args_tools.values()
+
+        if isinstance(args_tools, List):
+            # Add to our list in order, but without repeats
+            for tool in args_tools:
+                if tool not in tool_set:
+                    tool_set.add(tool)
+                    tool_list.append(tool)
+
+        return tool_list
