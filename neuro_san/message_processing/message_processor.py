e759623197f7e99aa2542b34e9c08e39af760c3e
Dan Fink <Daniel.Fink@cognizant.com>
2025-02-11 18:38:58 -0800

    Stab at MessageProcessor approach

---
+++ neuro_san/message_processing/message_processor.py
@@ -0,0 +1,40 @@
+
+# Copyright (C) 2023-2025 Cognizant Digital Business, Evolutionary AI.
+# All Rights Reserved.
+# Issued under the Academic Public License.
+#
+# You can be released from the terms, and requirements of the Academic Public
+# License by purchasing a commercial license.
+# Purchase of a commercial license is mandatory for any use of the
+# neuro-san SDK Software in commercial settings.
+#
+# END COPYRIGHT
+from typing import Any
+from typing import Dict
+
+from neuro_san.internals.messages.chat_message_type import ChatMessageType
+
+
+class MessageProcessor:
+    """
+    An interface for processing a single message.
+    """
+
+    def should_block_downstream_processing(self, chat_message_dict: Dict[str, Any],
+                                           message_type: ChatMessageType) -> bool:
+        """
+        :param chat_message_dict: The ChatMessage dictionary to consider.
+        :param message_type: The ChatMessageType of the chat_message_dictionary to process.
+        :return: True if the given message should be blocked from further downstream
+                processing.  False otherwise (the default).
+        """
+        _ = chat_message_dict, message_type
+        return False
+
+    def process_message(self, chat_message_dict: Dict[str, Any], message_type: ChatMessageType):
+        """
+        Process the message.
+        :param chat_message_dict: The ChatMessage dictionary to process.
+        :param message_type: The ChatMessageType of the chat_message_dictionary to process.
+        """
+        raise NotImplementedError
