dba1fa204a08b52e5b836abdc9b4a1c82d6218e0
Andrei Denissov <andrei.denissov@cognizant.com>
2025-03-12 15:25:09 -0700

    Change http request name.

--- neuro_san/http_sidecar/http_sidecar.py
+++ neuro_san/http_sidecar/http_sidecar.py
@@ -72,7 +72,7 @@ class HttpSidecar:
             {"agent_name": "concierge",
              "port": self.port,
              "forwarded_request_metadata": self.forwarded_request_metadata}
-        handlers.append(("/concierge", ConciergeHandler, concierge_data))
+        handlers.append(("/describe", ConciergeHandler, concierge_data))
 
         for agent_name in self.agents.keys():
             # For each of registered agents, we define 3 request paths -
