5e5a49f2b96a185c860c1a13b50743101d1a4d83
Andrei Denissov <andrei.denissov@cognizant.com>
2025-03-17 19:58:52 -0700

    Fix for race condition in async http request handler.

--- neuro_san/http_sidecar/handlers/streaming_chat_handler.py
+++ neuro_san/http_sidecar/handlers/streaming_chat_handler.py
@@ -15,6 +15,8 @@ See class comment for details
 from typing import Any, Dict, Generator
 import json
 
+import asyncio
+
 from google.protobuf.json_format import MessageToDict
 from google.protobuf.json_format import Parse
 
@@ -30,10 +32,32 @@ class StreamingChatHandler(BaseRequestHandler):
     Handler class for neuro-san streaming chat API call.
     """
 
+    async def stream_out(self,
+                         generator: Generator[Generator[ChatResponse, None, None], None, None]):
+        """
+        Process streaming out generator output to HTTP connection.
+        :param generator: async gRPC generator
+        """
+        # Set up headers for chunked response
+        self.set_header("Content-Type", "application/json")
+        self.set_header("Transfer-Encoding", "chunked")
+        # Flush headers immediately
+        await self.flush()
+
+        # async for result_message in result_generator:
+        # result_generator = self.async_generator()
+        async for sub_generator in generator:
+            async for result_message in sub_generator:
+                result_dict: Dict[str, Any] = MessageToDict(result_message)
+                result_str: str = json.dumps(result_dict) + "\n"
+                self.write(result_str)
+                await self.flush()
+
     async def post(self):
         """
         Implementation of POST request handler for streaming chat API call.
         """
+
         try:
             # Parse JSON body
             data = json.loads(self.request.body)
@@ -46,21 +70,7 @@ class StreamingChatHandler(BaseRequestHandler):
             # here we are getting Generator of Generators of ChatResponses!
             result_generator: Generator[Generator[ChatResponse, None, None], None, None] =\
                 grpc_session.streaming_chat(grpc_request)
-
-            # Set up headers for chunked response
-            self.set_header("Content-Type", "application/json")
-            self.set_header("Transfer-Encoding", "chunked")
-            # Flush headers immediately
-            await self.flush()
-
-            # async for result_message in result_generator:
-            # result_generator = self.async_generator()
-            async for sub_generator in result_generator:
-                async for result_message in sub_generator:
-                    result_dict: Dict[str, Any] = MessageToDict(result_message)
-                    result_str: str = json.dumps(result_dict) + "\n"
-                    self.write(result_str)
-                    await self.flush()
+            await self.stream_out(result_generator)
 
         except Exception as exc:  # pylint: disable=broad-exception-caught
             self.process_exception(exc)
