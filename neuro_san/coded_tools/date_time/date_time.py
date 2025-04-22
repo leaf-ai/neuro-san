16d99b362931213e654a31c6658a5e9f5e98e78c
Noravee Kanchanavatee <noraveek@gmail.com>
2025-04-22 01:21:55 -0700

    Add time zone and format for debugging

--- neuro_san/coded_tools/date_time/date_time.py
+++ neuro_san/coded_tools/date_time/date_time.py
@@ -1,4 +1,16 @@
+# Copyright (C) 2023-2025 Cognizant Digital Business, Evolutionary AI.
+# All Rights Reserved.
+# Issued under the Academic Public License.
+#
+# You can be released from the terms, and requirements of the Academic Public
+# License by purchasing a commercial license.
+# Purchase of a commercial license is mandatory for any use of the
+# neuro-san SDK Software in commercial settings.
+#
+# END COPYRIGHT
+
 from datetime import datetime
+from datetime import timezone
 from typing import Any
 from typing import Dict
 from typing import Union
@@ -11,7 +23,7 @@ class DateTime(CodedTool):
     CodedTool implementation which provides a current date and time
     """
 
-    def invoke(self, args: Dict[str, Any], sly_data: Dict[str, Any]) -> Union[Dict[str, Any], str]:
+    def invoke(self, args: Dict[str, Any], sly_data: Dict[str, Any]) -> str:
         """
         :param args: An argument dictionary whose keys are the parameters
                 to the coded tool and whose values are the values passed for them
@@ -40,8 +52,15 @@ class DateTime(CodedTool):
                 a text string an error message in the format:
                 "Error: <error message>"
         """
+        # Get current UTC time
+        now = datetime.now(timezone.utc)
 
-        return str(datetime.now())
+        # Format it in a user-friendly way
+        friendly_time = now.strftime("%A, %B %d, %Y at %I:%M %p %Z")
+
+        print("Current UTC date and time:", friendly_time)
+
+        return str(now)
 
     async def async_invoke(self, args: Dict[str, Any], sly_data: Dict[str, Any]) -> Union[Dict[str, Any], str]:
         raise NotImplementedError
