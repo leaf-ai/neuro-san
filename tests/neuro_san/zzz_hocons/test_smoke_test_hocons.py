a6d95ebc55159733b67ca6fe7d618fde11d48f3a
vincent.nguyen <vincent.nguyen@cognizant.com>
2025-05-14 16:03:23 -0700

    rename class

--- tests/neuro_san/zzz_hocons/test_smoke_test_hocons.py
+++ tests/neuro_san/zzz_hocons/test_smoke_test_hocons.py
@@ -18,7 +18,7 @@ from parameterized import parameterized
 from neuro_san.test.unittest.dynamic_hocon_unit_tests import DynamicHoconUnitTests
 
 
-class TestSmokeTestHocons(TestCase):
+class TestIntegrationTestHocons(TestCase):
     """
     Data-driven dynamic test cases where each test case is specified by a single hocon file.
     """
