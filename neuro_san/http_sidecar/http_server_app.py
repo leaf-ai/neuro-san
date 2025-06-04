9aa28ad644010585a58b75a0b276265fb1930180
Andrei Denissov <andrei.denissov@cognizant.com>
2025-06-03 19:58:59 -0700

    WIP.

--- neuro_san/http_sidecar/http_server_app.py
+++ neuro_san/http_sidecar/http_server_app.py
@@ -15,19 +15,41 @@ See class comment for details
 from typing import Any, Dict
 from tornado.web import Application
 
+from neuro_san.interfaces.event_loop_logger import EventLoopLogger
+
 
 class HttpServerApp(Application):
     """
     Class provides customized Tornado application for neuro-san service -
     with redefined internal logger so we can include custom request metadata.
     """
-    def __init__(self, handlers):
+    def __init__(self, handlers, requests_limit: int, logger: EventLoopLogger):
         """
         Constructor:
         :param handlers: list of request handlers
         """
         # Call the base constructor
         super().__init__(handlers=handlers)
+        self.total: int = 0
+        self.num_processing: int = 0
+        self.requests_stats: Dict[str, int] = {}
+        self.requests_limit: int = requests_limit
+        self.logger: EventLoopLogger = logger
+        self.serving: bool = True
+        self.shutdown_initiated: bool = False
+
+    def is_serving(self) -> bool:
+        print(">>>>>>>>>>>>> CHECK SERVING.")
+        return self.serving
+
+    def start_client_request(self, caller: str):
+        pass
+
+    def finish_client_request(self, metadata: Dict[str, Any], caller: str):
+        pass
+
+    def get_stats(self) -> str:
+        return ""
 
     def log_request(self, handler):
         request = handler.request
--- neuro_san/http_sidecar/http_sidecar.py
+++ neuro_san/http_sidecar/http_sidecar.py
@@ -21,6 +21,7 @@ from tornado.ioloop import IOLoop
 from neuro_san.interfaces.concierge_session import ConciergeSession
 from neuro_san.session.direct_concierge_session import DirectConciergeSession
 from neuro_san.service.agent_server import DEFAULT_FORWARDED_REQUEST_METADATA
+from neuro_san.interfaces.event_loop_logger import EventLoopLogger
 from neuro_san.http_sidecar.logging.http_logger import HttpLogger
 from neuro_san.http_sidecar.http_server_app import HttpServerApp
 
@@ -61,6 +62,7 @@ class HttpSidecar(AgentAuthorizer, AgentsUpdater):
         self.start_event: threading.Event = start_event
         self.port = port
         self.http_port = http_port
+        self.requests_limit: int = 10
         self.logger = None
         self.openapi_service_spec_path: str = openapi_service_spec_path
         self.forwarded_request_metadata: List[str] = forwarded_request_metadata.split(" ")
@@ -74,7 +76,7 @@ class HttpSidecar(AgentAuthorizer, AgentsUpdater):
         """
         self.lock = threading.Lock()
         self.logger = HttpLogger(self.forwarded_request_metadata)
-        app = self.make_app()
+        app = self.make_app(self.requests_limit, self.logger)
 
         # Wait for "go" signal which will be set by gRPC server and corresponding machinery
         # when everything is ready for servicing.
@@ -92,7 +94,7 @@ class HttpSidecar(AgentAuthorizer, AgentsUpdater):
 
         IOLoop.current().start()
 
-    def make_app(self):
+    def make_app(self, requests_limit: int, logger: EventLoopLogger):
         """
         Construct tornado HTTP "application" to run.
         """
@@ -111,7 +113,7 @@ class HttpSidecar(AgentAuthorizer, AgentsUpdater):
         handlers.append((r"/api/v1/([^/]+)/connectivity", ConnectivityHandler, request_data))
         handlers.append((r"/api/v1/([^/]+)/streaming_chat", StreamingChatHandler, request_data))
 
-        return HttpServerApp(handlers)
+        return HttpServerApp(handlers, requests_limit, logger)
 
     def allow(self, agent_name) -> bool:
         return self.allowed_agents.get(agent_name, False)
